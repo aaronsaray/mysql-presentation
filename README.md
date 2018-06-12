41d7fe3e064134cf47d8ccb966bad8e3a1d5e496
Aaron Saray <me@aaronsaray.com>
2018-06-12 15:48:36 -0500

    Updating the views presentation item

--- README.md
+++ README.md
@@ -19,4 +19,38 @@ The details of the presentation branch are different below on each branch.
 
 ### Presentation Details
 
-> Here are details for this particular presentation branch.
+#### MySQL Views
+
+Benefits:
+- complex queries over to simple statements
+- security - give access to only certain users
+- calculated columns 
+- backwards compatible - change underlying tables
+
+Concerns:
+- ORM
+- merge and temptable
+- indexes only on the underlying tables (can use temp table, filesort a lot)
+
+What to review:
+- `students` and `grades` tables
+- simple `select * from GradeA`; (merge)
+- `select s.name, v.grade from GradeA v inner join students s on s.id=v.student_id` can join on view
+
+- more complex `select * from StudentGrades`
+
+- Get average, `select avg(grade) from grades`
+- `SELECT s.name, g.grade, g.added FROM grades g inner join students s on s.id=g.student_id WHERE g.grade > (SELECT AVG(grade) FROM grades);`
+- `select * from AboveAverage;`
+- How many times above average? `select count(*), name from AboveAverage group by name;`
+
+- update using a view
+    - no aggregates, simple, no joins
+    - `SELECT table_name, is_updatable FROM information_schema.views;`
+    - `delete from GradeA where id=1;`
+    - `select * from grades`    
+
+
+Other topics to look into:
+- with check option: to make sure you don't update things outside of the view's purview
+- security apparatus: sql security definer vs invoker
---
+++ sql/dump.sql
@@ -0,0 +1,173 @@
+CREATE DATABASE  IF NOT EXISTS `mine` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mine`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary view structure for view `AboveAverage`
+--
+
+DROP TABLE IF EXISTS `AboveAverage`;
+/*!50001 DROP VIEW IF EXISTS `AboveAverage`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `AboveAverage` AS SELECT 
+ 1 AS `name`,
+ 1 AS `grade`,
+ 1 AS `added`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `GradeA`
+--
+
+DROP TABLE IF EXISTS `GradeA`;
+/*!50001 DROP VIEW IF EXISTS `GradeA`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `GradeA` AS SELECT 
+ 1 AS `id`,
+ 1 AS `student_id`,
+ 1 AS `grade`,
+ 1 AS `added`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `StudentGrades`
+--
+
+DROP TABLE IF EXISTS `StudentGrades`;
+/*!50001 DROP VIEW IF EXISTS `StudentGrades`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `StudentGrades` AS SELECT 
+ 1 AS `name`,
+ 1 AS `number_of_assignments`,
+ 1 AS `average_grade`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `grades`
+--
+
+DROP TABLE IF EXISTS `grades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `grade` tinyint(1) DEFAULT NULL,
+  `added` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `student_id_idx` (`student_id`),
+  CONSTRAINT `student` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grades`
+--
+
+LOCK TABLES `grades` WRITE;
+/*!40000 ALTER TABLE `grades` DISABLE KEYS */;
+INSERT INTO `grades` VALUES (1,1,5,'2018-06-12 18:47:44'),(2,2,5,'2018-06-12 18:48:08'),(3,3,5,'2018-06-12 18:48:13'),(4,2,4,'2018-06-12 18:49:06'),(5,1,3,'2018-06-12 18:49:09'),(6,3,5,'2018-06-12 18:49:14'),(7,2,1,'2018-06-12 18:49:17'),(8,2,1,'2018-06-12 18:49:19'),(9,2,1,'2018-06-12 18:49:21'),(10,3,2,'2018-06-12 18:49:26');
+/*!40000 ALTER TABLE `grades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Guy Smiley'),(2,'George Bush'),(3,'Ginger Snap');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `AboveAverage`
+--
+
+/*!50001 DROP VIEW IF EXISTS `AboveAverage`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `AboveAverage` AS select `s`.`name` AS `name`,`g`.`grade` AS `grade`,`g`.`added` AS `added` from (`grades` `g` join `students` `s` on((`s`.`id` = `g`.`student_id`))) where (`g`.`grade` > (select avg(`grades`.`grade`) from `grades`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `GradeA`
+--
+
+/*!50001 DROP VIEW IF EXISTS `GradeA`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=MERGE */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `GradeA` AS select `grades`.`id` AS `id`,`grades`.`student_id` AS `student_id`,`grades`.`grade` AS `grade`,`grades`.`added` AS `added` from `grades` where (`grades`.`grade` > 4) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `StudentGrades`
+--
+
+/*!50001 DROP VIEW IF EXISTS `StudentGrades`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `StudentGrades` AS select `s`.`name` AS `name`,count(0) AS `number_of_assignments`,avg(`g`.`grade`) AS `average_grade` from (`grades` `g` join `students` `s` on((`s`.`id` = `g`.`student_id`))) group by `g`.`student_id` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-12 14:59:24
