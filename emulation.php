aab90e5f0bfa386329b2b85c43deec17ef61183d
Aaron Saray <me@aaronsaray.com>
2018-06-18 12:25:06 -0500

    added spacing to make it easier to understand

--- emulation.php
+++ emulation.php
@@ -6,7 +6,7 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "select count(*) from items where type_1 = :type or type_2 = :type";
 $stmt = $pdo->prepare($sql);
 $stmt->execute(['type' => 'a']);
-print $stmt->fetchColumn(0);
+print $stmt->fetchColumn(0) . "\n\n";
 
 
 $pdo = new PDO('mysql:host=127.0.0.1;dbname=mine', 'root');
